f03d37b3dcdc5c9ee654ee5ad674652eda29840d
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-07-03 18:42:59 +0200

    Update site details, and add calculator fields

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -3,9 +3,28 @@ import './Calculator.css';
 import React from 'react';
 
 const Calculator = () => (
-  <div className="calculator">
-    Calculator
-  </div>
+  <section className="calculator">
+    <h2 className="result">0</h2>
+    <button type="button" className="Btn-text">AC</button>
+    <button type="button" className="Btn-text">+/-</button>
+    <button type="button" className="Btn-text">%</button>
+    <button type="button" className="Btn-text">/</button>
+    <button type="button" className="Btn-text">7</button>
+    <button type="button" className="Btn-text">8</button>
+    <button type="button" className="Btn-text">9</button>
+    <button type="button" className="Btn-text">*</button>
+    <button type="button" className="Btn-text">4</button>
+    <button type="button" className="Btn-text">5</button>
+    <button type="button" className="Btn-text">6</button>
+    <button type="button" className="Btn-text">-</button>
+    <button type="button" className="Btn-text">1</button>
+    <button type="button" className="Btn-text">2</button>
+    <button type="button" className="Btn-text">3</button>
+    <button type="button" className="Btn-text">+</button>
+    <button type="button" className="Btn-text">0</button>
+    <button type="button" className="Btn-text">.</button>
+    <button type="button" className="Btn-text">=</button>
+  </section>
 );
 
 export default Calculator;
