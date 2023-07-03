3dade377ff3928602ba573adb3c57e8ea27e66c1
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-07-03 23:53:30 +0200

    Create child component Buttons and move styles

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import './Calculator.css';
+import Buttons from '../Buttons/Buttons';
 
 const Calculator = () => (
   <section className="calculator">
     <h2 className="result">0</h2>
-    <button type="button" className="btn-text">AC</button>
-    <button type="button" className="btn-text">+/-</button>
-    <button type="button" className="btn-text">%</button>
-    <button type="button" className="btn-text orange">÷</button>
-    <button type="button" className="btn-text">7</button>
-    <button type="button" className="btn-text">8</button>
-    <button type="button" className="btn-text">9</button>
-    <button type="button" className="btn-text orange">*</button>
-    <button type="button" className="btn-text">4</button>
-    <button type="button" className="btn-text">5</button>
-    <button type="button" className="btn-text">6</button>
-    <button type="button" className="btn-text orange">-</button>
-    <button type="button" className="btn-text">1</button>
-    <button type="button" className="btn-text">2</button>
-    <button type="button" className="btn-text">3</button>
-    <button type="button" className="btn-text orange">+</button>
-    <button type="button" className="btn-text btn-0">0</button>
-    <button type="button" className="btn-text">.</button>
-    <button type="button" className="btn-text orange">=</button>
+    <Buttons />
   </section>
 );
 
