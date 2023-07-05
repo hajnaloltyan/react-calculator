5bde58376e0c78df647d09a146a0f03434a3731c
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-07-05 22:40:47 +0200

    Change fetchData to have loading and error state with timeout, and cleanup

--- src/components/Quotes/Quotes.js
+++ src/components/Quotes/Quotes.js
@@ -1,45 +1,74 @@
 import React, { useState, useEffect } from 'react';
-import 'dotenv/config';
+import env from 'react-dotenv';
 import './Quotes.css';
 
 const Quotes = () => {
   const [quote, setQuote] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [haveError, setHaveError] = useState(false);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await fetch('https://api.api-ninjas.com/v1/quotes?category=failure&limit=2', {
-          method: 'GET',
-          headers: { 'X-Api-Key': process.env.REACT_APP_NINJA_KEY },
-          contentType: 'application/json',
-        });
-        const data = await result.json();
+    const delay = 3000;
+
+    const fetchData = () => fetch('https://api.api-ninjas.com/v1/quotes?category=failure&limit=2', {
+      method: 'GET',
+      headers: { 'X-Api-Key': env.NINJA_KEY },
+      contentType: 'application/json',
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
         const quotesWithIds = data.map((quote, index) => ({
           ...quote,
           id: index + 1,
         }));
-        return setQuote(quotesWithIds);
-      } catch (error) {
-        return {
-          Response: 'False',
-          Error: error.message || 'Unexpected error',
-        };
-      }
+        setQuote(quotesWithIds);
+      })
+      .catch((error) => {
+        setHaveError(true);
+        throw error;
+      });
+
+    const timeout = setTimeout(() => {
+      fetchData()
+        .then(() => {
+          setIsLoading(false);
+        });
+    }, delay);
+
+    return () => {
+      clearTimeout(timeout);
     };
-    fetchData();
-  }, [setQuote]);
+  }, []);
 
   return (
     <article className="quotesContainer">
       <h2>Today&apos;s Quotes:</h2>
-      {quote.map((item) => (
-        <div key={item.id} className="quoteItem">
-          <p>
-            {`"${item.quote}"`}
-          </p>
-          <h4 className="author">{item.author}</h4>
+      {isLoading && (
+        <div className="loadingContainer">
+          <div className="loading" />
         </div>
-      ))}
+      )}
+
+      {haveError && (
+        <h3>
+          Something went wrong :(
+        </h3>
+      )}
+
+      {(!isLoading && !haveError) && (
+        quote.map((item) => (
+          <div key={item.id} className="quoteItem">
+            <p>
+              {`"${item.quote}"`}
+            </p>
+            <h4 className="author">{item.author}</h4>
+          </div>
+        )))}
     </article>
   );
 };
