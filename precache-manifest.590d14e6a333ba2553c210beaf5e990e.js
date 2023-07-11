4e8e6da0ab6090dfdc0c4f8c031d7f2759b19ce0
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-07-11 17:26:25 +0200

    Updates

---
+++ precache-manifest.590d14e6a333ba2553c210beaf5e990e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea58716aff4684603254",
+    "url": "/react-calculator/static/css/main.cc85c913.chunk.css"
+  },
+  {
+    "revision": "ea58716aff4684603254",
+    "url": "/react-calculator/static/js/main.2c801888.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  },
+  {
+    "revision": "419f88e8610941f2f80d",
+    "url": "/react-calculator/static/js/2.854b123b.chunk.js"
+  },
+  {
+    "revision": "abe71e08efb339368020b1b136f70bf1",
+    "url": "/react-calculator/index.html"
+  }
+];
