63c35814156d2469518f6670fed7ad6f57918a35
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-07-11 21:47:43 +0200

    Uploading build for render deploy

---
+++ build/precache-manifest.dbd866115509ca661b10ecd009ce5040.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "838e20fd36763331d15d",
+    "url": "/react-calculator/static/css/main.5b814de7.chunk.css"
+  },
+  {
+    "revision": "838e20fd36763331d15d",
+    "url": "/react-calculator/static/js/main.dcca2a1d.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  },
+  {
+    "revision": "a5465255911bbebd7ada",
+    "url": "/react-calculator/static/js/2.9df49c33.chunk.js"
+  },
+  {
+    "revision": "0bff459306cfb057b483ee40c9cd0bd0",
+    "url": "/react-calculator/index.html"
+  }
+];
